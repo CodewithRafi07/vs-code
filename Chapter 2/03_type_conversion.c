f22448beecc34cc2fdd0de18274935d4b68d9b3d
CodewithRafi07 <mdrafiofficial24@gmail.com>
2025-02-12 22:22:55 +0600

    r

---
+++ Chapter 2/03_type_conversion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int a = 9; // float a = 9
+    int b = 2;
+    float c = a/b;
+    // int and int = int
+    // int and float = float
+    // float and float = float
+    printf("The value of a/b is %f\n" , c);
+    return 0;
+}
+// Time 1;46
